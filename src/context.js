94576e58e6bc863b5649482591af7df08799ace8
hepohle <hectorpohle@gmail.com>
2021-07-24 12:01:21 -0300

    first commit

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import items from './data';
+//import items from './data';
+import Client from './Contentful';
 
 const RoomContext = React.createContext();
 
@@ -19,21 +20,33 @@ class RoomProvider extends Component {
         breakfast: false,
         pets: false
     };
+    //getData
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: "beachResortRoom",
+                order:'fields.price'
+            });
+            let rooms = this.formatData(response.items);
+            let featuredRooms = rooms.filter(room => room.featured === true);
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+            this.setState({
+                rooms, 
+                featuredRooms,
+                sortedRooms: rooms,
+                loading: false,
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            });
+        }catch (error) {
+            console.log(error);
+        }
+    }
 
     componentDidMount(){
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
-        let maxPrice = Math.max(...rooms.map(item => item.price));
-        let maxSize = Math.max(...rooms.map(item => item.size));
-        this.setState({
-            rooms, 
-            featuredRooms,
-            sortedRooms: rooms,
-            loading: false,
-            price: maxPrice,
-            maxPrice,
-            maxSize
-        });
+        this.getData()
     }
 
     formatData(items){
